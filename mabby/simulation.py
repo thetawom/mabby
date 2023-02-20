11f01af3beb5c893ead2d00e1737662d68f5e29e
Ethan Wu <ew2664@columbia.edu>
2023-02-19 21:03:11 -0500

    chore: add type annotations

--- mabby/simulation.py
+++ mabby/simulation.py
@@ -1,15 +1,21 @@
+from __future__ import annotations
+
 from collections import defaultdict
+from typing import DefaultDict, List, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.typing import NDArray
+
+from mabby import ArmSet, Bandit
 
 
 class Simulation:
-    def __init__(self, bandits, armset):
+    def __init__(self, bandits: List[Bandit], armset: ArmSet):
         self.bandits = bandits
         self.armset = armset
 
-    def run(self, trials, steps):
+    def run(self, trials: int, steps: int) -> SimStats:
         stats = SimStats(self, trials, steps)
         for bandit in self.bandits:
             for _ in range(trials):
@@ -17,7 +23,7 @@ class Simulation:
         stats.compile_all()
         return stats
 
-    def _run_trial(self, stats, bandit, steps):
+    def _run_trial(self, stats: SimStats, bandit: Bandit, steps: int) -> None:
         bandit.prime(len(self.armset), steps)
         for i in range(steps):
             choice = bandit.choose()
@@ -26,20 +32,24 @@ class Simulation:
             stats.update(bandit, i, choice, reward)
 
 
+SimStatOpt = DefaultDict[Bandit, Optional[NDArray[np.float64]]]
+SimStat = DefaultDict[Bandit, NDArray[np.float64]]
+
+
 class SimStats:
-    def __init__(self, simulation, trials, steps):
+    def __init__(self, simulation: Simulation, trials: int, steps: int):
         self._sim = simulation
         self._trials = trials
 
-        self.rewards = defaultdict(lambda: None)
-        self.optimality = defaultdict(lambda: None)
-        self.regret = defaultdict(lambda: None)
+        self.rewards: SimStatOpt = defaultdict(lambda: None)
+        self.optimality: SimStatOpt = defaultdict(lambda: None)
+        self.regret: SimStatOpt = defaultdict(lambda: None)
 
-        self._rewards = defaultdict(lambda: np.zeros(steps))
-        self._optimality = defaultdict(lambda: np.zeros(steps))
-        self._regret = defaultdict(lambda: np.zeros(steps))
+        self._rewards: SimStat = defaultdict(lambda: np.zeros(steps))
+        self._optimality: SimStat = defaultdict(lambda: np.zeros(steps))
+        self._regret: SimStat = defaultdict(lambda: np.zeros(steps))
 
-    def update(self, bandit, i, choice, reward):
+    def update(self, bandit: Bandit, i: int, choice: int, reward: float) -> None:
         best_arm = self._sim.armset.best_arm()
         self._rewards[bandit][i] += reward
         self._optimality[bandit][i] += int(best_arm == choice)
@@ -47,28 +57,28 @@ class SimStats:
             self._sim.armset[best_arm].mean - self._sim.armset[choice].mean
         )
 
-    def compile_all(self):
+    def compile_all(self) -> None:
         for bandit in self._sim.bandits:
             self.compile_for_bandit(bandit)
 
-    def compile_for_bandit(self, bandit):
+    def compile_for_bandit(self, bandit: Bandit) -> None:
         self.rewards[bandit] = self._rewards[bandit] / self._trials
         self.optimality[bandit] = self._optimality[bandit] / self._trials
         self.regret[bandit] = np.cumsum(self._regret[bandit] / self._trials)
 
-    def plot_rewards(self):
+    def plot_rewards(self) -> None:
         for i, (bandit, stats) in enumerate(self.rewards.items()):
             plt.plot(stats, label=bandit.name)
         plt.legend()
         plt.show()
 
-    def plot_optimality(self):
+    def plot_optimality(self) -> None:
         for i, (bandit, stats) in enumerate(self.optimality.items()):
             plt.plot(stats, label=bandit.name)
         plt.legend()
         plt.show()
 
-    def plot_regret(self):
+    def plot_regret(self) -> None:
         for i, (bandit, stats) in enumerate(self.regret.items()):
             plt.plot(stats, label=bandit.name)
         plt.legend()
