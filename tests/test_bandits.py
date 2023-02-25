c66fab5ad0123c1b0cb8952bb8fce6b5ae00b6d9
Ethan Wu <ew2664@columbia.edu>
2023-02-25 16:58:24 -0500

    MNT: refactor unit tests for EpsilonGreedyBandit

--- tests/test_bandits.py
+++ tests/test_bandits.py
@@ -7,17 +7,7 @@ from mabby.exceptions import BanditUsageError
 
 @pytest.fixture
 def rng(mocker):
-    return mocker.MagicMock()
-
-
-@pytest.fixture
-def rng_low(mocker):
-    return mocker.MagicMock(random=lambda: 0.1)
-
-
-@pytest.fixture
-def rng_high(mocker):
-    return mocker.MagicMock(random=lambda: 0.9)
+    return mocker.MagicMock(random=lambda: 0.5)
 
 
 class TestBandit:
@@ -27,8 +17,12 @@ class TestBandit:
     def patch_abstract_methods(self, mocker):
         mocker.patch.object(Bandit, "__abstractmethods__", new_callable=set)
 
-    @pytest.fixture(params=[{"name": None}, {"name": "bandit-name"}])
+    @pytest.fixture(params=[{}, {"name": "bandit-name"}])
     def valid_params(self, request):
+        return request.param
+
+    @pytest.fixture(params=[])
+    def invalid_params(self, request):
         return request.param
 
     @pytest.fixture
@@ -59,7 +53,11 @@ class TestBandit:
         return primed_bandit
 
     def test_init_sets_name(self, valid_params, bandit):
-        assert bandit._name == valid_params["name"]
+        assert bandit._name == valid_params.get("name")
+
+    def test_init_raises_error_with_invalid_params(self, invalid_params):
+        with pytest.raises(ValueError):
+            self.BANDIT_CLASS(**invalid_params)
 
     @pytest.mark.parametrize("name", ["name"])
     def test_name_returns_custom_name_if_overridden(self, valid_params, name):
@@ -120,77 +118,57 @@ class TestBandit:
             primed_bandit.update(reward=reward)
 
 
-@pytest.mark.parametrize("eps", [0.5])
-class TestEpsilonGreedyBandit:
-    @pytest.fixture
-    def rng_low(self, mocker):
-        return mocker.MagicMock(random=lambda: 0.1)
+class TestEpsilonGreedyBandit(TestBandit):
+    BANDIT_CLASS = EpsilonGreedyBandit
 
-    @pytest.fixture
-    def rng_high(self, mocker):
-        return mocker.MagicMock(random=lambda: 0.9)
+    @pytest.fixture(params=[{"eps": 0.1}, {"eps": 0.3, "name": "bandit-name"}])
+    def valid_params(self, request):
+        return request.param
 
-    def test_init_sets_eps_and_uses_default_name(self, eps):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        assert bandit.eps == eps
-        assert str(eps) in bandit.name
+    @pytest.fixture(params=[{"eps": -1}, {"eps": 1.2}])
+    def invalid_params(self, request):
+        return request.param
 
-    @pytest.mark.parametrize("invalid_eps", [-0.5, 1.2])
-    def test_init_with_invalid_eps_raises_error(self, invalid_eps, eps):
-        with pytest.raises(ValueError):
-            EpsilonGreedyBandit(eps=invalid_eps)
+    @pytest.fixture(params=[[0.1, 0.5, 0.2], [2, 0]])
+    def Qs(self, request):
+        return request.param
 
-    @pytest.mark.parametrize("name", ["bandit-name"])
-    def test_init_with_name_uses_custom_name(self, eps, name):
-        bandit = EpsilonGreedyBandit(eps=eps, name=name)
-        assert bandit.eps == eps
-        assert bandit.name == name
+    def test_default_name_contains_eps(self, valid_params, bandit):
+        eps = valid_params["eps"]
+        assert str(eps) in bandit.default_name()
 
-    def test_default_name_contains_eps(self, eps):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        default_name = bandit.default_name()
-        assert str(eps) in default_name
+    def test__prime_inits__Qs_and__Ns(self, prime_params, primed_bandit):
+        k = prime_params["k"]
+        assert isinstance(primed_bandit._Qs, np.ndarray)
+        assert isinstance(primed_bandit._Ns, np.ndarray)
+        assert len(primed_bandit._Qs) == k
+        assert len(primed_bandit._Ns) == k
+        assert not primed_bandit._Qs.any()
+        assert not primed_bandit._Ns.any()
 
-    @pytest.mark.parametrize("k", [3])
-    @pytest.mark.parametrize("steps", [10])
-    def test_prime_inits_Qs_and_Ns(self, eps, k, steps):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        bandit._prime(k, steps)
-        assert isinstance(bandit._Qs, np.ndarray)
-        assert isinstance(bandit._Ns, np.ndarray)
-        assert len(bandit._Qs) == k
-        assert len(bandit._Ns) == k
-        assert not bandit._Qs.any()
-        assert not bandit._Ns.any()
+    def test__choose_explores_with_low_rng(
+        self, mocker, valid_params, prime_params, primed_bandit
+    ):
+        rng = mocker.MagicMock(random=lambda: 0.9 * valid_params["eps"])
+        primed_bandit._choose(rng)
+        rng.integers.assert_called_once_with(0, prime_params["k"])
 
-    @pytest.mark.parametrize("k", [3])
-    def test_choose_with_low_rng_explores(self, eps, k, rng_low):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        bandit._prime(k, 10)
-        bandit._choose(rng_low)
-        rng_low.integers.assert_called_once_with(0, k)
-
-    @pytest.mark.parametrize("Qs", [[0.1, 0.5, 0.2], [2, 0]])
-    def test_choose_with_high_rng_exploits(self, eps, Qs, rng_high):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        bandit._Qs = Qs
-        choice = bandit._choose(rng_high)
+    def test__choose_exploits_with_high_rng(
+        self, mocker, valid_params, primed_bandit, Qs
+    ):
+        rng = mocker.MagicMock(random=lambda: 1.1 * valid_params["eps"])
+        primed_bandit._Qs = Qs
+        choice = primed_bandit._choose(rng)
         assert Qs[choice] == max(Qs)
 
-    @pytest.mark.parametrize("k", [3])
-    @pytest.mark.parametrize("choice", [0, 1])
-    @pytest.mark.parametrize("reward", [0.5])
-    def test_update_updates_Ns_and_Qs(self, eps, k, choice, reward):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        bandit._prime(k, 10)
-        bandit._Ns = np.ones(k)
-        bandit._update(choice, reward)
-        assert bandit._Ns[choice] == 2
-        assert bandit._Qs[choice] == reward / 2
+    def test__update_updates__Qs_and__Ns(
+        self, choice, prime_params, chosen_bandit, reward
+    ):
+        chosen_bandit._Ns = np.ones(prime_params["k"])
+        chosen_bandit._update(choice, reward)
+        assert chosen_bandit._Qs[choice] == reward / 2
+        assert chosen_bandit._Ns[choice] == 2
 
-    @pytest.mark.parametrize("Qs", [[0.1, 0.5, 0.2]])
-    def test_compute_Qs_returns_Qs(self, eps, Qs):
-        bandit = EpsilonGreedyBandit(eps=eps)
-        bandit._Qs = Qs
-        ret_Qs = bandit.compute_Qs()
-        assert ret_Qs == Qs
+    def test_compute_Qs_returns_Qs(self, primed_bandit, Qs):
+        primed_bandit._Qs = Qs
+        assert primed_bandit.compute_Qs() == primed_bandit._Qs
