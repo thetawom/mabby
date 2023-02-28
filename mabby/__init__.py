ebe96aad6c2e35ac6093374e490c8d22f187520c
Ethan Wu <ew2664@columbia.edu>
2023-02-28 18:44:14 -0500

    MNT: add ArmSet to top-level import

--- mabby/__init__.py
+++ mabby/__init__.py
@@ -1,8 +1,9 @@
-from mabby.arms import BernoulliArm, GaussianArm
+from mabby.arms import ArmSet, BernoulliArm, GaussianArm
 from mabby.bandits import Bandit, EpsilonGreedyBandit, RandomBandit
 from mabby.simulation import Simulation
 
 __all__ = [
+    "ArmSet",
     "BernoulliArm",
     "GaussianArm",
     "Bandit",
