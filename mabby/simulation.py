cf7e3db1b08f438bb532a35d816cfabdad8a805a
Ethan Wu <ew2664@columbia.edu>
2023-03-05 22:11:54 -0500

    MNT: refactor Simulation to use SimulationStats

--- mabby/simulation.py
+++ mabby/simulation.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
-from collections import defaultdict
 from collections.abc import Iterable
-from typing import TYPE_CHECKING, DefaultDict
+from typing import TYPE_CHECKING
 
-import matplotlib.pyplot as plt
 import numpy as np
-from numpy.typing import NDArray
 
 from mabby.bandits import Bandit
-from mabby.exceptions import SimulationUsageError
+from mabby.stats import BanditStats, SimulationStats
 
 if TYPE_CHECKING:
     from mabby.arms import ArmSet
@@ -25,86 +22,22 @@ class Simulation:
         self.armset = armset
         self._rng = np.random.default_rng(seed)
 
-    def run(self, trials: int, steps: int) -> SimStats:
-        stats = SimStats(self, trials, steps)
+    def run(self, trials: int, steps: int) -> SimulationStats:
+        sim_stats = SimulationStats(simulation=self)
         for bandit in self.bandits:
-            for _ in range(trials):
-                self._run_trial(stats, bandit, steps)
-        return stats.compile()
+            bandit_stats = self._run_trials_for_bandit(bandit, trials, steps)
+            sim_stats.add(bandit_stats)
+        return sim_stats
 
-    def _run_trial(self, stats: SimStats, bandit: Bandit, steps: int) -> None:
-        bandit.prime(len(self.armset), steps)
-        for i in range(steps):
-            choice = bandit.choose(self._rng)
-            reward = self.armset.play(choice, self._rng)
-            bandit.update(reward)
-            stats.update(bandit, i, choice, reward)
-
-
-SimStat = DefaultDict[Bandit, NDArray[np.float64]]
-
-
-class SimStats:
-    def __init__(self, simulation: Simulation, trials: int, steps: int):
-        self._sim = simulation
-        self._trials = trials
-
-        self._rewards: SimStat = defaultdict(lambda: np.zeros(steps))
-        self._optimality: SimStat = defaultdict(lambda: np.zeros(steps))
-        self._regret: SimStat = defaultdict(lambda: np.zeros(steps))
-
-        self._compiled = False
-
-    @property
-    def rewards(self) -> SimStat:
-        if not self._compiled:
-            raise SimulationUsageError("stats have not been compiled")
-        return self._rewards
-
-    @property
-    def optimality(self) -> SimStat:
-        if not self._compiled:
-            raise SimulationUsageError("stats have not been compiled")
-        return self._optimality
-
-    @property
-    def regret(self) -> SimStat:
-        if not self._compiled:
-            raise SimulationUsageError("stats have not been compiled")
-        return self._regret
-
-    def update(self, bandit: Bandit, i: int, choice: int, reward: float) -> None:
-        opt_arm = self._sim.armset.best_arm()
-        self._rewards[bandit][i] += reward
-        self._optimality[bandit][i] += int(opt_arm == choice)
-        self._regret[bandit][i] += (
-            self._sim.armset[opt_arm].mean - self._sim.armset[choice].mean
-        )
-        self._compiled = False
-
-    def compile(self) -> SimStats:
-        if not self._compiled:
-            for bandit in self._sim.bandits:
-                self._rewards[bandit] /= self._trials
-                self._optimality[bandit] /= self._trials
-                self._regret[bandit] = np.cumsum(self._regret[bandit] / self._trials)
-            self._compiled = True
-        return self
-
-    def plot_rewards(self) -> None:
-        for _, (bandit, stats) in enumerate(self.rewards.items()):
-            plt.plot(stats, label=bandit)
-        plt.legend()
-        plt.show()
-
-    def plot_optimality(self) -> None:
-        for _, (bandit, stats) in enumerate(self.optimality.items()):
-            plt.plot(stats, label=bandit)
-        plt.legend()
-        plt.show()
-
-    def plot_regret(self) -> None:
-        for _, (bandit, stats) in enumerate(self.regret.items()):
-            plt.plot(stats, label=bandit)
-        plt.legend()
-        plt.show()
+    def _run_trials_for_bandit(
+        self, bandit: Bandit, trials: int, steps: int
+    ) -> BanditStats:
+        bandit_stats = BanditStats(bandit, self.armset, steps)
+        for _ in range(trials):
+            bandit.prime(len(self.armset), steps)
+            for step in range(steps):
+                choice = bandit.choose(self._rng)
+                reward = self.armset.play(choice, self._rng)
+                bandit.update(reward)
+                bandit_stats.update(step, choice, reward)
+        return bandit_stats
