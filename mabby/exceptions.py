1afead72ca2eb64fb5d84f0bd14d52d4a1e05a1a
Ethan Wu <ew2664@columbia.edu>
2023-03-05 21:36:41 -0500

    feat: add SimulationStats tracker

--- mabby/exceptions.py
+++ mabby/exceptions.py
@@ -8,3 +8,9 @@ class SimulationUsageError(Exception):
     """Raised when simulation functions are called out of order"""
 
     pass
+
+
+class StatsUsageError(Exception):
+    """Raised when stats functions are called improperly"""
+
+    pass
