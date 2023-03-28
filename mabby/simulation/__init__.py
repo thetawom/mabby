05bd80885dede72cfa9cd8d1a447ca2ccc5e5472
Ethan Wu <ew2664@columbia.edu>
2023-03-27 22:55:49 -0400

    docs: add docstrings for simulation module

---
+++ mabby/simulation/__init__.py
@@ -0,0 +1,1 @@
+"""Provides simulation-related modules."""
--- mabby/utils.py
+++ mabby/utils.py
@@ -1,3 +1,4 @@
+"""Provides commonly used utility functions."""
 from __future__ import annotations
 
 import numpy as np
@@ -6,5 +7,16 @@ from numpy.typing import ArrayLike
 
 
 def random_argmax(values: ArrayLike, rng: Generator) -> int:
+    """Computes random argmax of an array.
+
+    If there are multiple maximums, the index of one is chosen at random.
+
+    Args:
+        values: An input array.
+        rng: A random number generator.
+
+    Returns:
+        The random argmax of the input array.
+    """
     candidates = np.where(values == np.max(values))[0]
     return int(rng.choice(candidates))
